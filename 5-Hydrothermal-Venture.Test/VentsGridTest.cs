d48aca94d1a179e0895d2839a79ea96a6b7e2d3d
Thomas <thomas.m.elbo@gmail.com>
2021-12-05 21:36:42 +0100

    added horizontalLineAdd and Vertical. Small test failes and is outcommented for now

--- 5-Hydrothermal-Venture.Test/VentsGridTest.cs
+++ 5-Hydrothermal-Venture.Test/VentsGridTest.cs
@@ -8,45 +8,95 @@ namespace _5_Hydrothermal_Venture.Test
     public class VentsGridTest
     {
         [TestMethod]
-        public void TestCreateVentsGrid()
+        public void TestVentsGridCreateVentsGrid()
         {
             VentsGrid ventsGrid = new VentsGrid();
 
-            int[][] ventsGridList = ventsGrid.getGrid();
+            List<List<int>> ventsGridList = ventsGrid.getGrid();
 
-            int vents = 0;
-            foreach (int[] row in ventsGridList)
-                row.Where(amountOfVents => amountOfVents > 0);
-            Assert.AreEqual(0, vents);
+            Assert.IsFalse(ventsGridList.Any());
         }
 
         [TestMethod]
-        public void TestParseLine()
+        public void TestVentsGridParseLine()
         {
             VentsGrid ventsGrid = new VentsGrid();
-            string line = "0,0 -> 0,9";
+            string line = "0,0 -> 9,0";
 
             var coordinates = ventsGrid.ParseLine(line);
 
             Assert.AreEqual(0, coordinates.startX);
             Assert.AreEqual(0, coordinates.startY);
-            Assert.AreEqual(0, coordinates.endX);
-            Assert.AreEqual(9, coordinates.endY);
+            Assert.AreEqual(9, coordinates.endX);
+            Assert.AreEqual(0, coordinates.endY);
+        }
+
+        [TestMethod]
+        public void TestVentsGridAddVentsLineHorizontal()
+        {
+            VentsGrid ventsGrid = new VentsGrid();
+
+            string line = "0,0 -> 9,0";
+            var coordinates = ventsGrid.ParseLine(line);
+
+            ventsGrid.AddVentsLine(coordinates);
+
+            List<List<int>> grid = ventsGrid.getGrid();
+
+            Assert.AreEqual(9, grid[0].Sum());
+        }
+
+        [TestMethod]
+        public void TestVentsGridAddVentsLineVertical()
+        {
+            VentsGrid ventsGrid = new VentsGrid();
+
+            string line = "0,0 -> 0,9";
+            var coordinates = ventsGrid.ParseLine(line);
+
+            ventsGrid.AddVentsLine(coordinates);
+
+            List<List<int>> grid = ventsGrid.getGrid();
+
+            Assert.AreEqual(1, grid[0].Sum());
+            Assert.AreEqual(1, grid[1].Sum());
+            Assert.AreEqual(1, grid[2].Sum());
+            Assert.AreEqual(1, grid[3].Sum());
+            Assert.AreEqual(1, grid[4].Sum());
+            Assert.AreEqual(1, grid[5].Sum());
+            Assert.AreEqual(1, grid[6].Sum());
+            Assert.AreEqual(1, grid[7].Sum());
+            Assert.AreEqual(1, grid[8].Sum());
+            Assert.AreEqual(1, grid[9].Sum());
         }
 
         /*[TestMethod]
-        public void TestAddLineToVentsGrid()
+        public void TestVentsGridSmallTest()
         {
+            string[] ventLinesInput = LoadTestFile("smallTest.txt");
+
             VentsGrid ventsGrid = new VentsGrid();
-            string line = "0,0 -> 0,9";
+            foreach(var ventLine in ventLinesInput)
+            {
+                var coordinates = ventsGrid.ParseLine(ventLine);
+                ventsGrid.AddVentsLine(coordinates);
+            }
 
-            ventsGrid.AddVentsLine(line);
+            List<List<int>> grid = ventsGrid.getGrid();
 
-            int[][] ventsGridList = ventsGrid.getGrid();
-            int vents = 0;
-            foreach (int[] row in ventsGridList)
-                row.Where(amountOfVents => amountOfVents > 0);
-            Assert.AreEqual(9, vents);
+            var positions = 0;
+            foreach(var gridLine in grid)
+            {
+                positions += gridLine.Where(position => position >= 2).Count();
+            }
+
+            Assert.AreEqual(5, positions);
         }*/
+
+        private string[] LoadTestFile(string path)
+        {
+            string[] input = System.IO.File.ReadAllLines(path);
+            return input;
+        }
     }
 }
--- 5-Hydrothermal-Venture/VentsGrid.cs
+++ 5-Hydrothermal-Venture/VentsGrid.cs
@@ -6,12 +6,9 @@ namespace _5_Hydrothermal_Venture
 {
     public class VentsGrid
     {
-        private int[][] Grid = new int[][] { };
-        public void AddVentsLine(string line)
-        {      
-        }
+        private List<List<int>> Grid = new List<List<int>>();
 
-        public int[][] getGrid()
+        public List<List<int>> getGrid()
         {
             return Grid;
         }
@@ -28,6 +25,67 @@ namespace _5_Hydrothermal_Venture
             int endY = int.Parse(endCoordinates[1]);
 
             return (startX, startY, endX, endY);
+        }
+
+        public void AddVentsLine((int startX, int startY, int endX, int endY) coordinates)
+        {
+            if (coordinates.startY == coordinates.endY)
+                AddHorizontalLine(coordinates);
+
+            if (coordinates.startX == coordinates.endX)
+                AddVerticalLine(coordinates);
+
+        }
+
+        private void AddVerticalLine((int startX, int startY, int endX, int endY) coordinates)
+        {
+            if (!VerticalLineExist(coordinates.endY))
+                CreateVerticalLines(coordinates.endY);
+
+            
+
+            for (int index = coordinates.startY; index <= coordinates.endY; index++)
+            {
+                if (!HorizontalLineExist(index, coordinates.endX))
+                    CreateHorizontalPositions(index, coordinates.endX);
+                Grid[index][coordinates.endX] += 1;
+            }
+        }
+
+        private void AddHorizontalLine((int startX, int startY, int endX, int endY) coordinates)
+        {
+            if (!VerticalLineExist(coordinates.endY))
+                CreateVerticalLines(coordinates.endY);
+
+            if(!HorizontalLineExist(coordinates.endY, coordinates.endX))
+                CreateHorizontalPositions(coordinates.endY, coordinates.endX);
+
+            for (int index = coordinates.startX; index < coordinates.endX; index++)
+                Grid[coordinates.startY][index] += 1;
+        }
+
+        private void CreateHorizontalPositions(int index, int endX)
+        {
+            var amountOfPositionsToAdd = endX - Grid[index].Count;
+            for (var indexToAdd = 0; indexToAdd <= amountOfPositionsToAdd; indexToAdd++)
+                Grid[index].Add(0);
+        }
+
+        private bool HorizontalLineExist(int endY, int endX)
+        {
+            return Grid[endY].Count - 1 >= endX;
+        }
+
+        private void CreateVerticalLines(int endY)
+        {
+            var amountOfLinesToAdd = endY - Grid.Count;
+            for (var indexToAdd = 0; indexToAdd <= amountOfLinesToAdd; indexToAdd++)
+                Grid.Add(new List<int>());
+        }
+
+        private bool VerticalLineExist(int endY)
+        {
+            return Grid.Count-1 >= endY;
         }
     }
 }
