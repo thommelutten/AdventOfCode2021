5a2448634fff54aef9350a1159b6a1cbe73bc9aa
Thomas <thomas.m.elbo@gmail.com>
2021-12-05 08:17:51 +0100

    Added task for day 5

---
+++ 5-Hydrothermal-Venture.Test/VentsGridTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _5_Hydrothermal_Venture.Test
+{
+    [TestClass]
+    public class VentsGridTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 5-Hydrothermal-Venture/VentsGrid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5_Hydrothermal_Venture
+{
+    class VentsGrid
+    {
+    }
+}
