591de270bfab6fe6e05dfd4ecd1198462bc9a831
Thomas <thomas.m.elbo@gmail.com>
2021-12-05 08:56:15 +0100

    Added a couple of initial tests

--- 5-Hydrothermal-Venture.Test/VentsGridTest.cs
+++ 5-Hydrothermal-Venture.Test/VentsGridTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _5_Hydrothermal_Venture.Test
 {
@@ -6,8 +8,45 @@ namespace _5_Hydrothermal_Venture.Test
     public class VentsGridTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCreateVentsGrid()
         {
+            VentsGrid ventsGrid = new VentsGrid();
+
+            int[][] ventsGridList = ventsGrid.getGrid();
+
+            int vents = 0;
+            foreach (int[] row in ventsGridList)
+                row.Where(amountOfVents => amountOfVents > 0);
+            Assert.AreEqual(0, vents);
         }
+
+        [TestMethod]
+        public void TestParseLine()
+        {
+            VentsGrid ventsGrid = new VentsGrid();
+            string line = "0,0 -> 0,9";
+
+            var coordinates = ventsGrid.ParseLine(line);
+
+            Assert.AreEqual(0, coordinates.startX);
+            Assert.AreEqual(0, coordinates.startY);
+            Assert.AreEqual(0, coordinates.endX);
+            Assert.AreEqual(9, coordinates.endY);
+        }
+
+        /*[TestMethod]
+        public void TestAddLineToVentsGrid()
+        {
+            VentsGrid ventsGrid = new VentsGrid();
+            string line = "0,0 -> 0,9";
+
+            ventsGrid.AddVentsLine(line);
+
+            int[][] ventsGridList = ventsGrid.getGrid();
+            int vents = 0;
+            foreach (int[] row in ventsGridList)
+                row.Where(amountOfVents => amountOfVents > 0);
+            Assert.AreEqual(9, vents);
+        }*/
     }
 }
--- 5-Hydrothermal-Venture/VentsGrid.cs
+++ 5-Hydrothermal-Venture/VentsGrid.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace _5_Hydrothermal_Venture
 {
-    class VentsGrid
+    public class VentsGrid
     {
+        private int[][] Grid = new int[][] { };
+        public void AddVentsLine(string line)
+        {      
+        }
+
+        public int[][] getGrid()
+        {
+            return Grid;
+        }
+
+        public (int startX, int startY, int endX, int endY) ParseLine(string line)
+        {
+            string[] parts = line.Split(' ');
+            string[] startCoordinates = parts[0].Split(',');
+            string[] endCoordinates = parts[2].Split(',');
+
+            int startX = int.Parse(startCoordinates[0]);
+            int startY = int.Parse(startCoordinates[1]);
+            int endX = int.Parse(endCoordinates[0]);
+            int endY = int.Parse(endCoordinates[1]);
+
+            return (startX, startY, endX, endY);
+        }
     }
 }
