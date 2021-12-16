35ef12002d50dc17d99f41f4f23f56f44486501b
Thomas <thomas.m.elbo@gmail.com>
2021-12-16 17:15:49 +0100

    Added day 13 info

---
+++ 13-Transparent-Origami.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _13_Transparent_Origami.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 13-Transparent-Origami/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13_Transparent_Origami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
