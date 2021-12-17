3238710034f53be932c8a00dff2e0a8dd1472142
Thomas <thomas.m.elbo@gmail.com>
2021-12-17 16:14:44 +0100

    Finished day 13 part 1, added test for part 2

--- 13-Transparent-Origami.Test/PaperTest.cs
+++ 13-Transparent-Origami.Test/PaperTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -36,6 +37,235 @@ namespace _13_Transparent_Origami.Test
             var dotsOnFirstRow = lines[0].Where(isMarked => isMarked).Count();
 
             Assert.AreEqual(3, dotsOnFirstRow);
+        }
+
+        [TestMethod]
+        public void TestPaperLoadInstructions()
+        {
+            Paper paper = new Paper();
+            var dotsAndFoldingInstructions = LoadDotsAndFoldingInstructionsFromFile("smallTest.txt");
+            paper.LoadFoldingInstructions(dotsAndFoldingInstructions.foldingInstructions);
+
+            var foldingInstructions = paper.FoldingInstructions;
+            Assert.AreEqual(2, foldingInstructions.Count);
+
+            Assert.AreEqual("y", foldingInstructions[0].axis);
+            Assert.AreEqual(7, foldingInstructions[0].lineNumber);
+
+            Assert.AreEqual("x", foldingInstructions[1].axis);
+            Assert.AreEqual(5, foldingInstructions[1].lineNumber);
+        }
+
+        [TestMethod]
+        public void TestPaperFoldHorizontal()
+        {
+            Paper paper = InitializePaper("smallTest.txt");
+
+            List<List<bool>> lines = paper.Lines;
+            var dotsOnRow = lines[0].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(3, dotsOnRow);
+
+            paper.FoldHorizontalAt(7);
+
+            lines = paper.Lines;
+            dotsOnRow = lines[0].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(5, dotsOnRow);
+            Assert.IsTrue(lines[0][0]);
+            Assert.IsTrue(lines[0][2]);
+            Assert.IsTrue(lines[0][3]);
+            Assert.IsTrue(lines[0][6]);
+            Assert.IsTrue(lines[0][9]);
+
+            dotsOnRow = lines[1].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+            Assert.IsTrue(lines[1][0]);
+            Assert.IsTrue(lines[1][4]);
+
+            dotsOnRow = lines[2].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+            Assert.IsTrue(lines[2][6]);
+            Assert.IsTrue(lines[2][10]);
+
+            dotsOnRow = lines[3].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+            Assert.IsTrue(lines[3][0]);
+            Assert.IsTrue(lines[3][4]);
+            
+            dotsOnRow = lines[4].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(6, dotsOnRow);
+            Assert.IsTrue(lines[4][1]);
+            Assert.IsTrue(lines[4][3]);
+            Assert.IsTrue(lines[4][6]);
+            Assert.IsTrue(lines[4][8]);
+            Assert.IsTrue(lines[4][9]);
+            Assert.IsTrue(lines[4][10]);
+
+
+            Assert.AreEqual(7, lines.Count);
+        }
+
+        [TestMethod]
+        public void TestPaperFoldVertical()
+        {
+            Paper paper = InitializePaper("smallTest.txt");
+            paper.FoldHorizontalAt(7);
+            paper.FoldVerticalAt(5);
+
+            List<List<bool>> lines = paper.Lines;
+            var dotsOnRow = lines[0].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(5, dotsOnRow);
+            Assert.IsTrue(lines[0][0]);
+            Assert.IsTrue(lines[0][1]);
+            Assert.IsTrue(lines[0][2]);
+            Assert.IsTrue(lines[0][3]);
+            Assert.IsTrue(lines[0][4]);
+            
+
+            dotsOnRow = lines[1].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+
+            dotsOnRow = lines[2].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+
+            dotsOnRow = lines[3].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+
+            dotsOnRow = lines[4].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(5, dotsOnRow);
+        }
+
+        [TestMethod]
+        public void TestPaperFold()
+        {
+            Paper paper = InitializePaper("smallTest.txt");
+
+            List<List<bool>> lines = paper.Lines;
+            var dotsOnFirstRow = lines[0].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(3, dotsOnFirstRow);
+
+            paper.Fold();
+
+            lines = paper.Lines;
+            dotsOnFirstRow = lines[0].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(5, dotsOnFirstRow);
+        }
+
+        [TestMethod]
+        public void TestPaperReverseLine()
+        {
+            List<bool> line = new List<bool> { false, false, true };
+
+            Paper paper = new Paper();
+
+            var reversedLine = paper.ReverseLine(line);
+
+            Assert.IsTrue(line[0]);
+            Assert.IsFalse(line[1]);
+            Assert.IsFalse(line[2]);
+        }
+
+        [TestMethod]
+        public void TestPaperReverseLines()
+        {
+            Paper paper = new Paper();
+
+            List<bool> line = new List<bool> { false, false, true };
+            List<bool> line2 = new List<bool> { true, false, true };
+            List<List<bool>> lines = new List<List<bool>>() { line, line2 };
+
+            lines = paper.ReverseLines(lines);
+
+            Assert.IsTrue(lines[0][0]);
+            Assert.IsFalse(lines[0][1]);
+            Assert.IsFalse(lines[0][2]);
+
+            Assert.IsTrue(lines[1][0]);
+            Assert.IsFalse(lines[1][1]);
+            Assert.IsTrue(lines[1][2]);
+        }
+
+        [TestMethod]
+        public void TestPaperFoldAllInstructions()
+        {
+            Paper paper = InitializePaper("smallTest.txt");
+            paper.FoldAllInstructions();
+
+            List<List<bool>> lines = paper.Lines;
+            var dotsOnRow = lines[0].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(5, dotsOnRow);
+
+            dotsOnRow = lines[1].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+
+            dotsOnRow = lines[2].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+
+            dotsOnRow = lines[3].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(2, dotsOnRow);
+
+            dotsOnRow = lines[4].Where(isMarked => isMarked).Count();
+            Assert.AreEqual(5, dotsOnRow);
+        }
+
+        [TestMethod]
+        public void TestPaperCountDots()
+        {
+            Paper paper = InitializePaper("smallTest.txt");
+            paper.FoldAllInstructions();
+
+            int dots = paper.CountDots();
+
+            Assert.AreEqual(16, dots);
+        }
+
+        [TestMethod]
+        public void TestPaperSingleFoldCountDotsFullTest()
+        {
+            Paper paper = InitializePaper("fullTest.txt");
+            paper.Fold();
+
+            int dots = paper.CountDots();
+
+            Console.WriteLine(dots);
+        }
+
+        [TestMethod]
+        public void TestPaperPrintCode()
+        {
+            Paper paper = InitializePaper("smallTest.txt");
+            paper.FoldAllInstructions();
+            List<string> code = paper.DecipherCode();
+            Assert.AreEqual("#####", code[0]);
+            Assert.AreEqual("#      #", code[1]);
+            Assert.AreEqual("#      #", code[2]);
+            Assert.AreEqual("#      #", code[3]);
+            Assert.AreEqual("#####", code[4]);
+            Assert.AreEqual("          ", code[5]);
+            Assert.AreEqual("          ", code[6]);
+
+            foreach (var decipheredLine in code)
+                Console.WriteLine(decipheredLine);
+        }
+
+        [TestMethod]
+        public void TestPaperDecipherCodeFullTest()
+        {
+            Paper paper = InitializePaper("fullTest.txt");
+            paper.FoldAllInstructions();
+
+            List<string> decipheredLines = paper.DecipherCode();
+
+            foreach (var decipheredLine in decipheredLines)
+                Console.WriteLine(decipheredLine);
+        }
+
+        private Paper InitializePaper(string path)
+        {
+            Paper paper = new Paper();
+            var dotsAndFoldingInstructions = LoadDotsAndFoldingInstructionsFromFile(path);
+            paper.LoadDots(dotsAndFoldingInstructions.dots);
+            paper.LoadFoldingInstructions(dotsAndFoldingInstructions.foldingInstructions);
+            return paper;
         }
 
         private (List<List<bool>> dots, List<(string axis, int lineNumber)> foldingInstructions) LoadDotsAndFoldingInstructionsFromFile(string path)
--- 13-Transparent-Origami/Paper.cs
+++ 13-Transparent-Origami/Paper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace _13_Transparent_Origami
@@ -7,10 +8,139 @@ namespace _13_Transparent_Origami
     public class Paper
     {
         public List<List<bool>> Lines { get; private set; } = new List<List<bool>>();
+        public List<(string axis, int lineNumber)> FoldingInstructions { get; set; }
 
         public void LoadDots(List<List<bool>> dots)
         {
             Lines = dots;
+        }
+
+        public void LoadFoldingInstructions(List<(string axis, int lineNumber)> foldingInstructions)
+        {
+            FoldingInstructions = foldingInstructions;
+        }
+
+        public void Fold()
+        {
+            var foldingInstruction = FoldingInstructions[0];
+
+            if (foldingInstruction.axis.Equals("x"))
+                FoldVerticalAt(foldingInstruction.lineNumber);
+            else
+                FoldHorizontalAt(foldingInstruction.lineNumber);
+
+            FoldingInstructions.RemoveAt(0);
+        }
+
+        public void FoldHorizontalAt(int rowNumberToFoldAt)
+        {
+            var reversedBottomPartOfPaper = new List<List<bool>>();
+
+            for(int paperLineIndex = Lines.Count-1; paperLineIndex > rowNumberToFoldAt; paperLineIndex--)
+            {
+                var paperLine = new List<bool>();
+                for (int positionOnLine = 0; positionOnLine < Lines[paperLineIndex].Count; positionOnLine++)
+                    paperLine.Add(Lines[paperLineIndex][positionOnLine]);
+                reversedBottomPartOfPaper.Add(paperLine);
+                Lines.RemoveAt(paperLineIndex);
+            }
+
+            Lines.RemoveAt(Lines.Count - 1);
+
+            for(int paperLineIndex = 0; paperLineIndex < reversedBottomPartOfPaper.Count; paperLineIndex++)
+            {
+                for(int positionOnLine = 0; positionOnLine < reversedBottomPartOfPaper[paperLineIndex].Count; positionOnLine++)
+                {
+                    if (Lines[paperLineIndex].Count <= positionOnLine)
+                        Lines[paperLineIndex].Add(false);
+
+                    if (Lines[paperLineIndex][positionOnLine])
+                        continue;
+
+                    Lines[paperLineIndex][positionOnLine] = reversedBottomPartOfPaper[paperLineIndex][positionOnLine];
+                }
+            }
+        }
+
+        public void FoldVerticalAt(int columnNumberToFoldAt)
+        {
+            var rightSideOfPaper = new List<List<bool>>();
+
+            for(int paperLineIndex = 0; paperLineIndex < Lines.Count; paperLineIndex++)
+            {
+                var paperLine = new List<bool>(new bool[columnNumberToFoldAt + 1]);
+                for(int positionOnLine = columnNumberToFoldAt; positionOnLine < Lines[paperLineIndex].Count; positionOnLine++)
+                {
+                    paperLine[(positionOnLine - columnNumberToFoldAt)] = Lines[paperLineIndex][positionOnLine];
+                }
+                rightSideOfPaper.Add(paperLine);
+            }
+
+            var reversedRightSideOfPaper = ReverseLines(rightSideOfPaper);
+
+            for(int paperlineIndex = 0; paperlineIndex < Lines.Count;  paperlineIndex++)
+            {
+                for(int positionOnLine = 0; positionOnLine < reversedRightSideOfPaper[paperlineIndex].Count; positionOnLine++)
+                {
+                    if (Lines[paperlineIndex].Count <= positionOnLine)
+                        Lines[paperlineIndex].Add(false);
+
+                    if (Lines[paperlineIndex][positionOnLine])
+                        continue;
+                    Lines[paperlineIndex][positionOnLine] = reversedRightSideOfPaper[paperlineIndex][positionOnLine];
+                }
+                while (Lines[paperlineIndex].Count != reversedRightSideOfPaper[paperlineIndex].Count - 1)
+                    Lines[paperlineIndex].RemoveAt(Lines[paperlineIndex].Count - 1);
+
+            }
+        }
+
+        public List<List<bool>> ReverseLines(List<List<bool>> lines)
+        {
+            List<List<bool>> reversedLines = new List<List<bool>>();
+            foreach (var line in lines)
+                reversedLines.Add(ReverseLine(line));
+            return reversedLines;
+        }
+
+        public List<bool> ReverseLine(List<bool> line)
+        {
+            line.Reverse();
+            return line;
+        }
+
+        public void FoldAllInstructions()
+        {
+            while (FoldingInstructions.Count > 0)
+                Fold();
+        }
+
+        public int CountDots()
+        {
+            int dots = 0;
+
+            foreach(var line in Lines)
+                dots += line.Where(isMarked => isMarked).Count();
+
+            return dots;
+        }
+
+        public List<string> DecipherCode()
+        {
+            var decipheredLines = new List<string>();
+            foreach(var line in Lines)
+            {
+                string decipheredLine = string.Empty;
+                foreach(var position in line)
+                {
+                    if (position)
+                        decipheredLine += "#";
+                    else
+                        decipheredLine += "  ";
+                }
+                decipheredLines.Add(decipheredLine);
+            }
+            return decipheredLines;
         }
     }
 }
